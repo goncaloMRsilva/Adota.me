97e1bf86e21c45327865619ee16c8ec65aa59eca
Gonçalo Silva <gmrs99.silva@gmail.com>
2022-06-27 19:32:07 +0200

    script de criacao e remocao das tabelas

--- SQL/createTables.sql
+++ SQL/createTables.sql
@@ -1,71 +1,76 @@
-create if not exists schema Adotame;
+create schema if not exists adotame;
 
-create table Adotame.User(
-    IDuser int Primary Key,
-    Name varchar(250) not null,
-    Email varchar(100) not null,
-    Phone int not null
+create table if not exists adotame.user(
+    id_user int primary key,
+    name varchar(250) not null,
+    email varchar(100) not null,
+    phone int not null
 );
 
-create table Adotame.Login(
-    IDlogin int Primary Key,
-    UserName varchar(250) not null,
-    Pass varchar(100) not null,
-    IDuser int references (User.IDuser)
+create table if not exists adotame.login(
+    id_login int primary key,
+    username varchar(250) not null,
+    password varchar(100) not null,
+    id_user int references adotame.user (id_user)
 );
 
-create table Adotame.Profile(
-    IDprofile int Primary Key,
-    Name varchar(250) not null,
-    IDlogin int references (Login.IDlogin)
+create table if not exists adotame.profile(
+    id_profile int primary key,
+    name varchar(250) not null,
+    id_login int references adotame.login (id_login)
 );
 
-create table Adotame.Permission(
-    IDpermission int Primary Key,
-    Name varchar(250) not null,
+create table if not exists adotame.login_profile(
+    id_login int references adotame.login (id_login),
+    id_profile int references adotame.profile (id_profile)
 );
 
-create table Adotame.ProfilePermission(
-    IDpermission int references (Permission.IDpermission)
-    IDprofile int references (Profile.IDprofile)
+create table if not exists adotame.permission(
+    id_permission int primary key,
+    name varchar(250) not null
 );
 
-create table Adotame.Catalog(
-    IDcatalog int Primary Key,
-    Name varchar(250) not null
+create table if not exists adotame.profile_permission(
+    id_permission int references adotame.permission (id_permission),
+    id_profile int references adotame.profile (id_profile)
 );
 
-create table Adotame.Animal(
-    IDanimal int Primary Key,
-    Name varchar(100) not null,
-    Type varchar(100) not null,    /* cao ou gato?*/
-    Photo varchar(500) not null,
-    Gender varchar(100) not null,
-    Age int (10) not null,
-    Size varchar (100) not null,
-    Fur varchar(100) not null,    /*pelagem*/
-    Breed varchar(300) not null,   /*raca*/
-    Color varchar(250) null,
-    Vaccines varchar(500) null,
-    Portion varchar(300) null,
-    State varchar(500) not null,
-    Cares varchar(1000) not null
+create table if not exists adotame.catalog(
+    id_catalog int primary key,
+    name varchar(250) not null
 );
 
-create table Adotame.CatalogAnimal(
-    IDanimal int references (Animal.IDanimal),
-    IDcatalog int references (Catalog.IDcatalog)
+create table if not exists adotame.animal(
+    id_animal int primary key,
+    name varchar(100) not null,
+    type varchar(100) not null,    /* cao ou gato?*/
+    photo varchar(500) not null,
+    gender varchar(100) not null,
+    age int not null,
+    size varchar (100) not null,
+    fur varchar(100) not null,    /*pelagem*/
+    breed varchar(300) not null,   /*raca*/
+    color varchar(250) null,
+    vaccines varchar(500) null,
+    portion varchar(300) null,
+    state varchar(500) not null,
+    cares varchar(1000) not null
 );
 
-create table Adotame.RequestType(
-    IDrequestType int Primary Key,
-    Name varchar(250) not null
+create table if not exists adotame.catalog_animal(
+    id_animal int references adotame.animal (id_animal),
+    id_catalog int references adotame.catalog (id_catalog)
 );
 
-create table Adotame.Request(
-    IDrequest int Primary Key,
-    UserName varchar(250) not null,
-    DateRequest date not null,
-    IDuser int references (User.IDuser),
-    IDrequestType int references (RequestType.IDrequestType)
+create table if not exists adotame.request_type(
+    id_request_type int primary key,
+    name varchar(250) not null
+);
+
+create table if not exists adotame.request(
+    id_request int primary key,
+    username varchar(250) not null,
+    date_request date not null,
+    id_user int references adotame.user (id_user),
+    id_request_type int references adotame.request_type (id_request_type)
 );
---
+++ SQL/removetables.sql
@@ -0,0 +1,12 @@
+drop table adotame.profile_permission;
+drop table adotame.catalog_animal;
+drop table adotame.login_profile;
+
+drop table adotame.login;
+drop table adotame.user;
+drop table adotame.profile;
+drop table adotame.permission;
+drop table adotame.catalog;
+drop table adotame.animal;
+drop table adotame.request;
+drop table adotame.request_type;
