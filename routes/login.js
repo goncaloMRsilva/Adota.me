46bf3c95a6f2154812d6b7ed74b47f5e6b992a68
Gonçalo Silva <gmrs99.silva@gmail.com>
2022-07-04 13:46:31 +0200

    Estrutura inicial do projeto

---
+++ routes/login.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login/index', { title: 'Express' });
+});
+
+router.get('/update/:id', function(req, res, next) {
+    res.render('login/create-update', { id: req.params.id });
+  });
+
+  router.get('/create', function(req, res, next) {
+    res.render('login/create-update', { id : null });
+  });
+
+  router.get('/list', function(req, res, next) {
+    res.render('login/list', { title: 'Express' });
+  });
+
+module.exports = router;
