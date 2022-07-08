017a464085afa4e8e1e8900bd56701f7503864a7
Gonçalo Silva <gmrs99.silva@gmail.com>
2022-07-08 19:19:04 +0200

    atualizacao das pk para tipo uuid. seedData atualizado mas nao completo

--- SQL/createTables.sql
+++ SQL/createTables.sql
@@ -1,47 +1,47 @@
 create schema if not exists adotame;
 
 create table if not exists adotame.user(
-    id_user bigint primary key,
+    id_user uuid primary key,
     name varchar(250) not null,
     email varchar(100) not null,
     phone bigint not null
 );
 
 create table if not exists adotame.login(
-    id_login bigint primary key,
+    id_login uuid primary key,
     username varchar(250) not null,
     password varchar(100) not null,
-    id_user bigint references adotame.user (id_user)
+    id_user uuid references adotame.user (id_user)
 );
 
 create table if not exists adotame.profile(
-    id_profile bigint primary key,
+    id_profile uuid primary key,
     name varchar(250) not null,
-    id_login bigint references adotame.login (id_login)
+    id_login uuid references adotame.login (id_login)
 );
 
 create table if not exists adotame.login_profile(
-    id_login bigint references adotame.login (id_login),
-    id_profile bigint references adotame.profile (id_profile)
+    id_login uuid references adotame.login (id_login),
+    id_profile uuid references adotame.profile (id_profile)
 );
 
 create table if not exists adotame.permission(
-    id_permission bigint primary key,
+    id_permission uuid primary key,
     name varchar(250) not null
 );
 
 create table if not exists adotame.profile_permission(
-    id_permission bigint references adotame.permission (id_permission),
-    id_profile bigint references adotame.profile (id_profile)
+    id_permission uuid references adotame.permission (id_permission),
+    id_profile uuid references adotame.profile (id_profile)
 );
 
 create table if not exists adotame.catalog(
-    id_catalog bigint primary key,
+    id_catalog uuid primary key,
     name varchar(250) not null
 );
 
 create table if not exists adotame.animal(
-    id_animal bigint primary key,
+    id_animal uuid primary key,
     name varchar(100) not null,
     type varchar(100) not null,    /* cao ou gato?*/
     photo varchar(500) not null,
@@ -58,19 +58,19 @@ create table if not exists adotame.animal(
 );
 
 create table if not exists adotame.catalog_animal(
-    id_animal bigint references adotame.animal (id_animal),
-    id_catalog bigint references adotame.catalog (id_catalog)
+    id_animal uuid references adotame.animal (id_animal),
+    id_catalog uuid references adotame.catalog (id_catalog)
 );
 
 create table if not exists adotame.request_type(
-    id_request_type bigint primary key,
+    id_request_type uuid primary key,
     name varchar(250) not null
 );
 
 create table if not exists adotame.request(
-    id_request bigint primary key,
+    id_request uuid primary key,
     username varchar(250) not null,
     date_request date not null,
-    id_user bigint references adotame.user (id_user),
-    id_request_type bigint references adotame.request_type (id_request_type)
+    id_user uuid references adotame.user (id_user),
+    id_request_type uuid references adotame.request_type (id_request_type)
 );
