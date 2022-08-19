9db2a19c316ba700776d0b9980c1a1b325ff96a8
Gonçalo Silva <gmrs99.silva@gmail.com>
2022-08-19 11:34:44 +0200

    Quando é criado um novo animal, os dados deste são guardados na base de dados

--- routes/animal.js
+++ routes/animal.js
@@ -20,24 +20,26 @@ router.get('/create', function(req, res, next) {
 router.post('/create', function(req, res, next) {
   var name = req.body.name;
   console.log(name);
+
+  var type = req.body.type;
+  console.log(type);
   
-  db.one(`insert into adotame.animal(id_animal, name, type, photo, gender, age, size, fur, breed, color, vaccines, portion, health, cares)
-  values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) returning id_animal`,
-  [crypto.randomUUID(), req.body.name, req.body.type, req.body.photo, req.body.gender, req.body.age, req.body.size, req.body.fur,
-    req.body.breed, req.body.color, req.body.vaccines, req.body.portion, req.body.health, req.body.cares]).then(data => {
-      console.log(data.id_animal);
-    }).catch(error => {
-      console.log('ERROR:', error);
-    });
-
-  res.send("OK");
-
   if((name.length >= 15) || (name.length < 2)) {
     res.send("Name is not valid");
   }else{
     res.send("OK");
   }
+  
+  db.one(`insert into adotame.animal(id_animal, name, type, photo, gender, age, size, fur, breed, color, vaccines, portion, health, cares)
+  values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)`,
+  [crypto.randomUUID(), req.body.name, req.body.type, req.body.photo, req.body.gender, req.body.age, req.body.size, req.body.fur,
+    req.body.breed, req.body.color, req.body.vaccines, req.body.portion, req.body.health, req.body.cares]).then(rows => {
+      console.log(rows);
+    }).catch(error => {
+      console.log('ERROR:', error);
+    });
 
+  res.send("OK");
 });
 
 
