ea2eac14c84ff17d22f6a176970a0e3dcd761da7
Gonçalo Silva <gmrs99.silva@gmail.com>
2022-09-02 11:01:25 +0200

    Início da estrutura para o layout da  página principal

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This is home page' });
+  res.render('index', { layout: 'index' });
 });
 
 module.exports = router;
